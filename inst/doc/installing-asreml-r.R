74172fdff97f1b31f341d6e4af87953e767e9da0
Sam Rogers <biometrytraining@adelaide.edu.au>
2023-07-19 08:50:02 +0000

    version 1.1.3

---
+++ inst/doc/installing-asreml-r.R
@@ -0,0 +1,33 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  eval = FALSE
+)
+
+## -----------------------------------------------------------------------------
+#  install.packages("biometryassist")
+#  biometryassist::install_asreml()
+#  asreml::asreml.license.activate()
+
+## -----------------------------------------------------------------------------
+#  install.packages("biometryassist")
+
+## -----------------------------------------------------------------------------
+#  library(biometryassist)
+
+## -----------------------------------------------------------------------------
+#  if(!require(rlang) | !require(curl)) install.packages(c("rlang", "curl"))
+#  source("https://raw.githubusercontent.com/biometryhub/biometryassist/main/R/install_asreml.R")
+
+## -----------------------------------------------------------------------------
+#  install_asreml()
+
+## -----------------------------------------------------------------------------
+#  library(asreml)
+#  asreml.license.activate()
+
+## -----------------------------------------------------------------------------
+#  update_asreml()
+#  install_asreml(force = TRUE) # Equivalent to the above
+
